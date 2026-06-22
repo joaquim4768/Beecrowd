2149700b368052be199a7bbc785237862d3fe7c3
Joaquim Andrade <166303550+Tharos4768@users.noreply.github.com>
2026-06-22 11:13:45 -0300

    1037 js

---
+++ Javascript/B1037/1037.js
@@ -0,0 +1,20 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var valor = Number(lines.shift());
+
+if(valor < 0 || valor > 100){
+    console.log(`Fora de intervalo`);
+}
+else if(valor >= 0 && valor <= 25){
+    console.log(`Intervalo [0,25]`);
+}
+else if(valor > 25 && valor <= 50){
+    console.log(`Intervalo (25,50]`);
+}
+else if(valor > 50 && valor <= 75){
+    console.log(`Intervalo (50,75]`);
+}
+else if(valor > 75 && valor <= 100){
+    console.log(`Intervalo (75,100]`);
+}
